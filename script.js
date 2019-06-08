702087d733320d1e901fecdcd423ecda0be2e689
Roman <ukkitop@gmail.com>
2019-06-08 23:32:45 +0300

    fix input additional numbers, fix clear table after result

--- script.js
+++ script.js
@@ -1,128 +1,159 @@
 const buttonsArray = document.querySelectorAll(".btnNumber");
-buttonsArray.forEach(function (btn)  {
-    
-    btn.addEventListener("click" , function()  {        
-      let input = document.querySelector("#input");
-      let numbersStack;
-      numbersStack= input.getAttribute("value");
-      input.setAttribute("value",numbersStack != null ? numbersStack + btn.innerHTML: btn.innerHTML);
-      
-      
-    });
-})
+let previousKey = "";
+buttonsArray.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    let input = document.querySelector("#input");
+    if (previousKey === "="){
+      input.setAttribute(
+        "value", "");
+        previousKey = "";
+    }    
+    let numbersStack;
+    numbersStack = input.getAttribute("value");
+    input.setAttribute(
+      "value",
+      numbersStack != null ? numbersStack + btn.innerHTML : btn.innerHTML
+    );
+  });
+});
 
 const OperatorsKeys = document.querySelectorAll(".btnOperation");
 let firstNumber = 0;
 let operatorPressed;
-OperatorsKeys.forEach(function (operator)  {
-    
-  operator.addEventListener("click" , function()  {        
+
+OperatorsKeys.forEach(function(operator) {
+  operator.addEventListener("click", function() {
     let input = document.querySelector("#input");
-    firstNumber = input.getAttribute("value");
-    operatorPressed = operator.innerHTML;    
-    input.setAttribute("value", "");    
-    
+    firstNumber = input.getAttribute("value");    
+    operatorPressed = operator.innerHTML;
+    input.setAttribute("value", "");
   });
-})
+});
 
 const ResultKey = document.querySelector(".result");
 
-ResultKey.addEventListener ("click", function() {
-  console.log(firstNumber)
-  console.log(input.getAttribute("value"))
-  console.log(operatorPressed)
-  switch (operatorPressed){
+ResultKey.addEventListener("click", function() {
+  console.log(firstNumber);
+  console.log(input.getAttribute("value"));
+  console.log(operatorPressed);
+  switch (operatorPressed) {
     case "+":
-      console.log("+")
-        input.setAttribute("value", parseFloat(firstNumber)  + parseFloat(input.getAttribute("value")));
-        break;
+      //console.log("+");
+      input.setAttribute(
+        "value",
+        parseFloat(firstNumber) + parseFloat(input.getAttribute("value"))
+      );
+      break;
     case "-":
-        console.log("-")
-        input.setAttribute("value", parseFloat(firstNumber)  - parseFloat(input.getAttribute("value")));
-        break;
+      //console.log("-");
+      input.setAttribute(
+        "value",
+        parseFloat(firstNumber) - parseFloat(input.getAttribute("value"))
+      );
+      break;
     case "/":
-        console.log("/")
-        input.setAttribute("value", parseFloat(firstNumber)  / parseFloat(input.getAttribute("value")));
-        break;
+      //console.log("/");
+      input.setAttribute(
+        "value",
+        parseFloat(firstNumber) / parseFloat(input.getAttribute("value"))
+      );
+      break;
     case "*":
-        console.log("*")
-        input.setAttribute("value", parseFloat(firstNumber)  * parseFloat(input.getAttribute("value")));
-        break;
+      //console.log("*");
+      input.setAttribute(
+        "value",
+        parseFloat(firstNumber) * parseFloat(input.getAttribute("value"))
+      );
+      break;
   }
-})
+  console.log("result:" + input.getAttribute("value"));
+  previousKey = "=";
+});
 
+const ResetKey = document.querySelector(".Reset");
 
-const ResetKey = document.querySelector(".Reset")
-
-ResetKey.addEventListener("click", function(){
+ResetKey.addEventListener("click", function() {
   let input = document.querySelector("#input");
   operatorPressed = "";
   firstNumber = "";
   input.setAttribute("value", "");
-})
-
+});
 
 const negativeKey = document.querySelector(".negative");
 
-negativeKey.addEventListener("click", function(){
+negativeKey.addEventListener("click", function() {
   let input = document.querySelector("#input");
-  if (input.getAttribute("value") != null && input.getAttribute("value").length != 0)
-  {
-    input.setAttribute("value", parseFloat(input.getAttribute("value")) * (-1)); 
+  if (
+    input.getAttribute("value") != null &&
+    input.getAttribute("value").length != 0
+  ) {
+    input.setAttribute("value", parseFloat(input.getAttribute("value")) * -1);
   }
-})
-
-
-document.addEventListener("keyup", function(e)  {
-  e = e || window.event;
-  console.log(isFinite(e.key));
-  console.log(e.key);
-  if (isFinite(e.key))
-  {
-      let input = document.querySelector("#input");
-      let numbersStack;
-      numbersStack= input.getAttribute("value");
-      input.setAttribute("value",numbersStack != null ? numbersStack + e.key: e.key);
-  }
-  switch(e.key){
-    case "+":
-      document.getElementById("add").click();
-    break
-    case "-":
-        document.getElementById("sub").click();
-    break
-    case "/":
-        document.getElementById("div").click();
-    break
-    case "*":
-        document.getElementById("mult").click();
-    break
-    case "Enter":
-        document.getElementById("result").click();
-    break
-    case ".":
-        document.getElementById("decimal").click();
-    break
-    case "Backspace":
-        document.getElementById("deleteLast").click();
-    break
-  }
-
 });
 
+document.addEventListener("keyup", function(e) {
+  e = e || window.event;
+  document.getElementById("input").focus();
+  if (isFinite(e.key)) {
+    document.getElementById(e.key).click();
+  }
+  console.log(e.key);
+  switch (e.key) {
+    case "+":
+      document.getElementById("add").click();
+      break;
+    case "-":
+      document.getElementById("sub").click();
+      break;
+    case "/":
+      document.getElementById("div").click();
+      break;
+    case "*":
+      document.getElementById("mult").click();
+      break;
+    case "Enter":
+      document.getElementById("result").click();
+      break;
+    case ".":
+      document.getElementById("decimal").click();
+      break;
+    case "Backspace":
+      document.getElementById("deleteLast").click();
+      break;
+  }
+});
 
 const DeleteLastKey = document.querySelector(".deleteLast");
 
-DeleteLastKey.addEventListener("click", function(){
+DeleteLastKey.addEventListener("click", function() {
   let inputString = input.getAttribute("value").toString();
-  let resultString = inputString.substr(0, inputString.length-1);
+  let resultString = inputString.substr(0, inputString.length - 1);
   console.log(resultString);
-  if (resultString === "-"){
-    input.setAttribute("value",""); 
+  if (resultString === "-") {
+    input.setAttribute("value", "");
+  } else {
+    input.setAttribute("value", resultString);
   }
-  else{
-    input.setAttribute("value",resultString)
+});
+
+var inactivityTime = function() {
+  var time;
+  window.onload = resetTimer;
+  
+  document.onmousemove = resetTimer;
+  document.onkeypress = resetTimer;
+
+  function logout() {
+    document.getElementById("Reset").click();
+    alert("inactive");
+    
   }
-})
 
-
+  function resetTimer() {
+    clearTimeout(time);
+    time = setTimeout(logout, 3600000);
+    // 1 hour timeout
+  }
+};
+/*call the function*/
+inactivityTime();
